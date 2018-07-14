5737341e8931c902e8f42cdb2fb38dfebf9db025
mac <chiheb.nabil@247labs.com>
2018-07-14 15:19:19 +0100

    add films post type

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -5,3 +5,18 @@ add_action( 'wp_enqueue_scripts', 'unite_child_enqueue_styles' );
 function unite_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+
+function create_film_post_type() {
+    register_post_type( 'films',
+      array(
+        'labels' => array(
+          'name' => __( 'Films' ),
+          'singular_name' => __( 'Film' )
+        ),
+        'public' => true,
+        'has_archive' => true,
+      )
+    );
+  }
+  add_action( 'init', 'create_film_post_type' );
